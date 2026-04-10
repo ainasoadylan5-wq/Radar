56a1b11e7ebe38fe587bfbd9ef13e2910280b160
ainasoadylan5-wq <ainasoadylan5@gmail.com>
2026-04-10 16:04:15 +0300

    Delete index.php

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['data'])) {
-    $data = $_POST['data'];
-    file_put_contents("frape.txt", $data . "\n", FILE_APPEND);
-    echo "OK";
-} else {
-    echo "Serveur Render opérationnel - en attente de données POST";
-}
-?>
