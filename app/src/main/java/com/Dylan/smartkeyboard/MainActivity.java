c3ec7412f864a1d357e9d8b202011e4a0ac49d2e
ainasoadylan5-wq <ainasoadylan5@gmail.com>
2026-04-10 16:25:37 +0300

    Create MainActivity.java

---
+++ app/src/main/java/com/Dylan/smartkeyboard/MainActivity.java
@@ -0,0 +1,14 @@
+package com.dylan.smartkeyboard;
+
+import android.app.*;
+import android.os.*;
+
+public class MainActivity extends Activity 
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
